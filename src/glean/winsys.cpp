eefec4caf3ee2b85d2ceb8731d729994060011a6
Allen Akin <akin@users.sourceforge.net>
2000-11-04 01:29:47 +0000

    When running in comparison mode, don't open the X11 display

--- src/glean/winsys.cpp
+++ src/glean/winsys.cpp
@@ -47,6 +47,16 @@ namespace GLEAN {
 ///////////////////////////////////////////////////////////////////////////////
 #if defined(__X11__)
 WindowSystem::WindowSystem(Options& o) {
+	// If running in "compare" mode we never actually use the window
+	// system, so we don't initialize it here.  This allows us to run
+	// on systems without graphics hardware/software.
+	if (o.mode == Options::compare) {
+		dpy = 0;
+		GLXVersMajor = GLXVersMinor = 0;
+		vip = 0;
+		return;
+	}
+
 	// Open the X11 display:
 	dpy = XOpenDisplay(o.dpyName.c_str());
 	if (!dpy)
