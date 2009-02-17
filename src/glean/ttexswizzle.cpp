f2dc736c8651061ed8a84f54a41515bfafb2f447
Brian Paul <brianp@users.sourceforge.net>
2009-02-17 22:11:09 +0000

    in ttexswizzle.cpp define the new GL tokens if GL_EXT_texture_swizzle is not in glext.h
    This allows the test to compile without requiring the latest glext.h header.

--- src/glean/ttexswizzle.cpp
+++ src/glean/ttexswizzle.cpp
@@ -45,6 +45,18 @@
 #include "image.h"
 
 
+// In case glext.h isn't new enough:
+#ifndef GL_EXT_texture_swizzle
+
+#define GL_TEXTURE_SWIZZLE_R_EXT          0x8E42
+#define GL_TEXTURE_SWIZZLE_G_EXT          0x8E43
+#define GL_TEXTURE_SWIZZLE_B_EXT          0x8E44
+#define GL_TEXTURE_SWIZZLE_A_EXT          0x8E45
+#define GL_TEXTURE_SWIZZLE_RGBA_EXT       0x8E46
+
+#endif
+
+
 namespace GLEAN {
 
 
